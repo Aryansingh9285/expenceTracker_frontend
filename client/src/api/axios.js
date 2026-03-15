cabcc1aea55dd544f1298ca7d26c8767db5b807e
Aryan Singh <as.aryansingh2004@gmail.com>
2026-03-15 21:32:23 +0530

    Update the Api EndPoint

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 const api=axios.create({
- baseURL: "https://expencetracker-backend-1lx9.onrender.com/api"
-})
+ baseURL: "import.meta.env.VITE_API_URL/api"
+})  
 
 api.interceptors.request.use((config)=>{
  const token=localStorage.getItem("token")
